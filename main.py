2104fcb2ee0459eceedf01687c8ebdcd75695c92
theCamelCaseGuy <129174506+TheCamelCaseGuy@users.noreply.github.com>
2023-11-03 16:55:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello, World")
+import polymer
+import threading
+
+runner = True
+config = {"defaultWaitTime": 1}
+
+def alwaysWait():
+    while runner:
+        polymer.Shell.wait(config["defaultWaitTime"])
+
+def echo(parameters):
+    print(" ".join(parameters[1:]))
+
+threading.Thread(target=alwaysWait).start()
+
+polymer.Shell.new("echo", echo)
+
+# Your Code Here -->
+
+polymer.Shell.start()
+runner = False
